fc8fe409fa0e335089588bc1143e962fe8222f26
Roman <roma.polyakov.2000@gmail.com>
2020-12-31 20:27:09 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
