933859e45ac6daae0489a85297582533c771947c
Roman <roma.polyakov.2000@gmail.com>
2020-12-31 17:07:23 +0300

    depl

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const path = require("path");
 
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
 
-//Routes//
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
+
+//Routes
 app.use("/auth", require("./routes/jwtAuth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
@@ -18,8 +24,8 @@ app.use("/booking", require("./routes/booking"));
 
 app.use("/reservations", require("./routes/reservations"));
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-	console.log(`Server has started on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Server has started on port ${PORT}`);
 });
